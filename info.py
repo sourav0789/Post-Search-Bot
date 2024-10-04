03e2f04e4196c5abf678dc7f4a04b840cb7006c6
sourav0789 <souravbosu3487@gmail.com>
2024-10-04 11:54:11 +0600

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
+API_ID       = ""
 API_HASH     = "99dff6c471774c2be6ff3d4623a6c0c0"
 BOT_TOKEN    = ""
 SESSION      = ""
 DATABASE_URI = "mongodb+srv://jhnbsomers0uh14:rVz5pWHzELN6aJWU@cluster0.sloz4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-LOG_CHANNEL  = 
-ADMIN        = 
+LOG_CHANNEL  = ""
+ADMIN        = ""
 CHANNEL      = "bdmoviezonepro"
